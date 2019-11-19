959bbdf22aee6f9a83fcacf0ad9d26e3bff664f0
jofas <jonas@fc-web.de>
2019-11-19 13:24:59 +0000

    output of test.sh from out/ -> tests/

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
-rm out/map.pgm
+rm tests/map.pgm
 
 make
 
 mpirun --hostfile hostfile -n $2 \
-  $1 -p 3 --pgm_file_path "out/map.pgm"
+  $1 -p 3 --pgm_file_path "tests/map.pgm"
 
 echo Testing $1
 
-if [ -f out/map.pgm ]; then
-  if [ "$(diff out/map.pgm out/true.pgm)" == "" ]; then
+if [ -f tests/map.pgm ]; then
+  if [ "$(diff tests/map.pgm tests/true.pgm)" == "" ]; then
     echo SUCCESS
   else
     echo FAILURE
